e4cd3b55d5c8de42898838bb70a51620907c273d
Jackson Miller <jackson@jackson.codes>
2025-01-12 17:34:12 -0500

    remove DIRECT_DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import { type Config } from "drizzle-kit";
 
-const databaseUrl = process.env.DATABASE_DIRECT_URL;
+const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
-  throw new Error("DATABASE_DIRECT_URL is not set");
+  throw new Error("DATABASE_URL is not set");
 }
 
 export default {
